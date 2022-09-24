a1d01bd5d3aa828d5aed82f97add6f28851caea6
Giovani Rojas Pinet <giovaniarp@gmail.com>
2022-09-24 12:16:43 -0700

    Scaffold of EHD project, include Domain, App and WebApi

---
+++ EasyHelpDesk/EHD.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHD.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        // Replace T with model
+        //DbSet<T> Products { get; set; }
+        Task<int> SaveChanges();
+    }
+}
